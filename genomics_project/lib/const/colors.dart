3d49487b5be808d41f816e29dbbacabcc3bf80b9
hp <nairaali109@std.mans.edu.eg>
2022-04-27 01:15:54 +0200

    colors.dart

--- genomics_project/lib/const/colors.dart
+++ genomics_project/lib/const/colors.dart
@@ -1,3 +1,13 @@
 import 'dart:ui';
-const Color lightBlue = Color(0xffaabfe9);
 
+const Color red = Color(0xffFF1B1B);
+const Color lightBlue = Color(0xff707070);
+const Color grey = Color(0xffAFAFAF);
+const Color white = Color(0xffF9F9F9);
+const Color white2 = Color(0xffEEEEEE);
+const Color lightBlue = Color(0xff99D1FF);
+const Color blue1 = Color(0xff007EFF);
+const Color blue2 = Color(0xff006DB0);
+const Color blue3 = Color(0xff074568);
+const Color blue4 = Color(0xff031F35);
+const Color darkBlue = Color(0xff020F19);
